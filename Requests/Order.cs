4942dc1b20a3030d9cf77836214059e1b9317537
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-11-12 16:39:14 -0300

    Remove documentation for now (I don't have time for that anyways)

--- Requests/Order.cs
+++ Requests/Order.cs
@@ -2,20 +2,8 @@
 
 namespace Iida.Shared.Requests;
 
-/// <summary>
-/// Order request.
-/// </summary>
 public class Order {
-	/// <summary>
-	/// GeoJSON polygon of the crop.
-	/// </summary>
 	public FeatureCollection? FeatureCollection { get; set; }
-	/// <summary>
-	/// Request start date.
-	/// </summary>
 	public DateTimeOffset? Start { get; set; }
-	/// <summary>
-	/// Request end date.
-	/// </summary>
 	public DateTimeOffset? End { get; set; }
 }
