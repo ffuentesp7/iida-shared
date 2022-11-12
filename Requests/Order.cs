d14a80e2e198526b3d6b1ab8eb9adbe22ab2abf7
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-11-11 22:32:08 -0300

    Order request

---
+++ Requests/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Iida.Shared.Requests;
+
+/// <summary>
+/// Order request.
+/// </summary>
+public class Order {
+	/// <summary>
+	/// GeoJSON polygon of the crop.
+	/// </summary>
+	public string? GeoJsonString { get; set; }
+	/// <summary>
+	/// Request start date.
+	/// </summary>
+	public DateTimeOffset? Start { get; set; }
+	/// <summary>
+	/// Request end date.
+	/// </summary>
+	public DateTimeOffset? End { get; set; }
+}
