ffe402dc4549725eeface28ab53a1fc61744d6b0
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-11-14 15:04:46 -0300

    Order model and Dto. Switched temp to home folder.

---
+++ DataTransferObjects/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+using GeoJSON.Net.Feature;
+
+namespace Iida.Shared.DataTransferObjects;
+
+public class Order {
+	[JsonPropertyName("geoJson"), Required]
+	public FeatureCollection? GeoJson { get; set; }
+	[JsonPropertyName("fecha_inicio"), Required]
+	public DateOnly? Start { get; set; }
+	[JsonPropertyName("fecha_fin"), Required]
+	public DateOnly? End { get; set; }
+	[JsonPropertyName("porcentaje_cobertura_nubes"), Required]
+	public string? CloudCover { get; set; }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using GeoJSON.Net.Feature;
+
+namespace Iida.Shared.Models;
+
+[Table("order")]
+public class Order {
+	[Column("id", Order = 0), Key, Required]
+	public int Id { get; set; }
+	[Column("guid", Order = 0), Required]
+	public Guid Guid { get; set; }
+	[Column("geo_json", Order = 0), Required]
+	public FeatureCollection? GeoJson { get; set; }
+	[Column("start", Order = 0), Required]
+	public DateOnly? Start { get; set; }
+	[Column("end", Order = 0), Required]
+	public DateOnly? End { get; set; }
+	[Column("cloud_cover", Order = 0), Required]
+	public string? CloudCover { get; set; }
+}
