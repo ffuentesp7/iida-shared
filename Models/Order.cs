7c91eb91164e176c57af8ef4918a9073724d0687
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-11-15 12:06:41 -0300

    Forgot to add cloud_cover property in Order model

--- Models/Order.cs
+++ Models/Order.cs
@@ -15,9 +15,11 @@ public class Order {
 	public DateTimeOffset? Start { get; set; }
 	[Column("end", Order = 4), Required]
 	public DateTimeOffset? End { get; set; }
-	[Column("eta_maps_fk", Order = 5)]
+	[Column("cloud_cover", Order = 5), Required]
+	public string? CloudCover { get; set; }
+	[Column("eta_maps_fk", Order = 6)]
 	public virtual ICollection<EvapotranspirationMap>? EvapotranspirationMaps { get; set; }
-	[Column("meteo_data_fk", Order = 6)]
+	[Column("meteo_data_fk", Order = 7)]
 	public virtual ICollection<MeteorologicalData>? MeteorologicalDatas { get; set; }
 	[Column("version"), ConcurrencyCheck]
 	public Guid Version { get; set; }
